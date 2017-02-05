149f5c95a23bef2eff4de2449104b711ef579f35
Laurits <laurits00@gmail.com>
2017-02-05 13:20:38 +0100

    Tilføjet forside. Linker til animals

--- wullum/wullum/urls.py
+++ wullum/wullum/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from home.views import index
 
 urlpatterns = [
+    url(r'^$', index),
     url(r'^animals/', include('animals.urls')),
     url(r'^admin/', admin.site.urls),
 ]
